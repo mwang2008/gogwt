45c2e0ee7ef41b4f40d0d1a6b857423365deed0c
allhotelmotel <allhotelmotel@1e24c64f-693a-f468-3ac7-b5590d3b4d65>
2010-08-13 19:08:42 +0000

    Add keyword DAO and unit test.

--- gwtbooking/src/main/java/com/gogwt/app/booking/businessService/domainService/CommonBusinessService.java
+++ gwtbooking/src/main/java/com/gogwt/app/booking/businessService/domainService/CommonBusinessService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.gogwt.app.booking.businessService.dataaccess.CommonDAO;
 import com.gogwt.app.booking.dto.dataObjects.UserContextBean;
 import com.gogwt.app.booking.dto.dataObjects.common.HotelBean;
+import com.gogwt.app.booking.dto.dataObjects.common.KeywordBean;
 import com.gogwt.app.booking.dto.dataObjects.common.StateBean;
 
 public class CommonBusinessService {
@@ -18,6 +19,11 @@ public class CommonBusinessService {
 		return getCommonDAO().getStateList(userContext);
 	}
 
+	public List<KeywordBean> getKeywordList(String keyword, final int numberResult) {
+		return getCommonDAO().getKeywordList(keyword, numberResult);
+	}
+
+	
 	public HotelBean getHotelDetail(final int propertyId, final UserContextBean userContext) {
 		return getCommonDAO().getHotelDetail(propertyId, userContext);
 	}
--- gwtbooking/src/main/java/com/gogwt/app/booking/rpc/services/reservation/HotelSearchRPCController.java
+++ gwtbooking/src/main/java/com/gogwt/app/booking/rpc/services/reservation/HotelSearchRPCController.java
@@ -1,10 +1,12 @@
 package com.gogwt.app.booking.rpc.services.reservation;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import com.gogwt.app.booking.businessService.domainService.LookupBusinessService;
 import com.gogwt.app.booking.businessService.domainService.ReservationBusinessService;
 import com.gogwt.app.booking.dto.dataObjects.UserContextBean;
+import com.gogwt.app.booking.dto.dataObjects.common.KeywordBean;
 import com.gogwt.app.booking.dto.dataObjects.request.SearchFormBean;
 import com.gogwt.app.booking.dto.dataObjects.response.HotelSearchResponseBean;
 import com.gogwt.app.booking.dto.dataObjects.response.SuggestiveDestinationResponseBean;
@@ -16,8 +18,25 @@ public class HotelSearchRPCController extends ReservationProcessServiceAdapter {
 	public ArrayList<SuggestiveDestinationResponseBean> getLocationKeyWords(
 			String destination, UserContextBean userContext)
 			throws AppRemoteException {
-		ArrayList<SuggestiveDestinationResponseBean> retList = new ArrayList<SuggestiveDestinationResponseBean>();
 		
+		List<KeywordBean> keywordList = 
+		    LookupBusinessService.getCommonBusinessService().getKeywordList(destination, 10);
+		
+		
+		ArrayList<SuggestiveDestinationResponseBean> retList = new ArrayList<SuggestiveDestinationResponseBean>();
+
+		if (keywordList == null || keywordList.isEmpty() ) {
+			return null;
+		}
+		
+		SuggestiveDestinationResponseBean suggestiveDestination = null;
+		for (KeywordBean keywordBean : keywordList) {
+			suggestiveDestination = new SuggestiveDestinationResponseBean();
+			suggestiveDestination.setSuggestedDestination(keywordBean.getKeyword());
+			retList.add(suggestiveDestination);
+		}
+		
+		/*
 		SuggestiveDestinationResponseBean suggestiveDestination = new SuggestiveDestinationResponseBean();		
 		suggestiveDestination.setSuggestedDestination("Atlanta");
 		retList.add(suggestiveDestination);
@@ -41,7 +60,8 @@ public class HotelSearchRPCController extends ReservationProcessServiceAdapter {
 		suggestiveDestination = new SuggestiveDestinationResponseBean();
 		suggestiveDestination.setSuggestedDestination("Atlanta downtown");
 		retList.add(suggestiveDestination);
-
+        */
+		
 		return retList;
 	}
 	
