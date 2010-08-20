7fdf5abb534dbe3746a297cf66c6503a7340336f
allhotelmotel <allhotelmotel@1e24c64f-693a-f468-3ac7-b5590d3b4d65>
2010-08-20 14:05:38 +0000

    add ui binder.

--- skeleton_gwtapp/src/main/java/com/skeleton/example/gwt/client/MyFirstGWTEntryPoint.java
+++ skeleton_gwtapp/src/main/java/com/skeleton/example/gwt/client/MyFirstGWTEntryPoint.java
@@ -1,20 +1,32 @@
 package com.skeleton.example.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
-import com.google.gwt.user.client.ui.HTML;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class MyFirstGWTEntryPoint implements EntryPoint {
 	private static TagsResources tags = TagsResources.Util.getInstance();
 	
+	//private static final Binder binder = GWT.create(Binder.class);
+	//interface Binder extends UiBinder<DockLayoutPanel, MyFirstGWTEntryPoint> { }
+	
 	@Override
 	public void onModuleLoad() {
-		String welcome = tags.label_welcome();
+	/*	
+	    String welcome = tags.label_welcome();
 		Label label = new Label(" HI this is my first GWT");
 		RootPanel.get().add(label);
 		RootPanel.get().add(new HTML("<b>" + welcome + "</b>"));
+	*/
 		
+		MyBinderWidget w = new MyBinderWidget();
+        RootPanel.get().add(w);
+
+		
+		// Create the UI defined in MyFirstGWTEntryPoint.ui.xml.
+	   // DockLayoutPanel outer = binder.createAndBindUi(this);
+	    
+	    //RootLayoutPanel root = RootLayoutPanel.get();
+	    //root.add(outer);
 	}
 
 }
