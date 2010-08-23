25a4c5dc975d664812cef05021630a191a39171a
allhotelmotel <allhotelmotel@1e24c64f-693a-f468-3ac7-b5590d3b4d65>
2010-08-23 14:12:38 +0000

    css for uiBinder

--- skeleton_gwtapp/src/main/java/com/skeleton/example/gwt/client/FavoriteColorWidget.java
+++ skeleton_gwtapp/src/main/java/com/skeleton/example/gwt/client/FavoriteColorWidget.java
@@ -7,15 +7,19 @@ import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.ListBox;
+import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 
 public class FavoriteColorWidget extends Composite {
-	 
+	private static TagsResources tags = TagsResources.Util.getInstance();
+	
     private static FavoriteColorWidgetUiBinder uiBinder = GWT
               .create(FavoriteColorWidgetUiBinder.class);
 
@@ -24,14 +28,34 @@ public class FavoriteColorWidget extends Composite {
     }
 
     @UiField Label greeting;
+    
     @UiField CheckBox red;
     @UiField CheckBox white;
     @UiField CheckBox blue;
     @UiField Button button;
-
+    @UiField ListBox listBox;
+    @UiField TextBox address;
+    
+    @UiField Label addressLabel;
+    
+    @UiField Label firstName;
+    //@UiField Label lastName;
+    @UiField Button btnSearch;
+    
     public FavoriteColorWidget() {
          initWidget(uiBinder.createAndBindUi(this));
 
+         addressLabel.setText(tags.Label_address());
+         
+         //list:
+         String[] nameList = {"michael", "steve", "bill"};
+         for (String name : nameList) { 
+             listBox.addItem(name); 
+         }
+
+         firstName.setText(tags.label_First_Name());
+         //lastName.setText(tags.label_Last_Name());
+         
          // add a greeting
          greeting.setText("Hello Jeff!!");
 
@@ -56,6 +80,16 @@ public class FavoriteColorWidget extends Composite {
 
     }
 
+    @UiHandler("btnSearch")
+    void handleClick(ClickEvent e) {
+    	//String address = address.get
+    	String whiteStr= white.getFormValue();
+    	String whiteText = white.getText();
+    	Boolean whiteBoolean = white.getValue();
+    	String addressStr = address.getText();
+    	
+      Window.alert("Hello, AJAX: whiteStr="+whiteStr + ", addressStr="+addressStr + ", whiteText="+whiteText + ",whiteBoolean="+whiteBoolean);
+    }
 
 
 }
--- skeleton_gwtapp/src/main/java/com/skeleton/example/gwt/client/MyFirstGWTEntryPoint.java
+++ skeleton_gwtapp/src/main/java/com/skeleton/example/gwt/client/MyFirstGWTEntryPoint.java
@@ -2,6 +2,7 @@ package com.skeleton.example.gwt.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.skeleton.example.gwt.client.TagsResources;
 
 public class MyFirstGWTEntryPoint implements EntryPoint {
 	private static TagsResources tags = TagsResources.Util.getInstance();
@@ -9,13 +10,14 @@ public class MyFirstGWTEntryPoint implements EntryPoint {
 	//private static final Binder binder = GWT.create(Binder.class);
 	//interface Binder extends UiBinder<DockLayoutPanel, MyFirstGWTEntryPoint> { }
 	
-	@Override
+	 
 	public void onModuleLoad() {
 	/*	
 	    String welcome = tags.label_welcome();
 		Label label = new Label(" HI this is my first GWT");
 		RootPanel.get().add(label);
 		RootPanel.get().add(new HTML("<b>" + welcome + "</b>"));
+		
 	*/
 		
 		MyBinderWidget w = new MyBinderWidget();
