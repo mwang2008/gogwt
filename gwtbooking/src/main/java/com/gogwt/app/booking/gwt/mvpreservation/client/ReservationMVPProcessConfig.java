d41473e6ebd62864045fba13d9d4b2dffec59694
allhotelmotel <allhotelmotel@1e24c64f-693a-f468-3ac7-b5590d3b4d65>
2010-08-30 19:51:29 +0000

    MVP module and uiBinder.

---
+++ gwtbooking/src/main/java/com/gogwt/app/booking/gwt/mvpreservation/client/ReservationMVPProcessConfig.java
@@ -0,0 +1,16 @@
+package com.gogwt.app.booking.gwt.mvpreservation.client;
+
+import com.gogwt.framework.arch.navigation.PageConfigAccessor;
+
+
+/**
+ * 
+ * ReservationProcessConfig must be the same name for the xml file defined 
+ * under resources/conf/gwt/ReservationMVPProcessConfig.xml
+ * 
+ * @author WangM
+ *
+ */
+public interface ReservationMVPProcessConfig extends PageConfigAccessor {
+
+}
---
+++ gwtbooking/src/main/java/com/gogwt/app/booking/gwt/mvpreservation/client/widgets/common/view/HeaderLayoutWidget.java
@@ -0,0 +1,124 @@
+package com.gogwt.app.booking.gwt.mvpreservation.client.widgets.common.view;
+
+import static com.gogwt.app.booking.dto.dataObjects.GWTPageConstant.HYPHON;
+import static com.gogwt.app.booking.dto.dataObjects.GWTPageConstant.SLASH;
+import static com.gogwt.app.booking.dto.dataObjects.GWTPageConstant.UNDER_SCORE;
+
+import com.gogwt.app.booking.gwt.common.utils.GWTExtClientUtils;
+import com.gogwt.app.booking.gwt.common.utils.WidgetStyleUtils;
+import com.gogwt.app.booking.gwt.mvpreservation.client.i18n.TagsReservationLookupResources;
+import com.gogwt.app.booking.gwt.mvpreservation.client.i18n.TagsReservationResources;
+import com.gogwt.framework.arch.utils.GWTStringUtils;
+import com.gogwt.framework.arch.widgets.BaseWidget;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Image;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.Panel;
+
+public class HeaderLayoutWidget extends BaseWidget {
+	private TagsReservationResources tags = TagsReservationResources.Util.getInstance();
+	
+	private Panel layoutPanel = WidgetStyleUtils.createHorizontalPanel();
+
+	public HeaderLayoutWidget() {
+		super();
+		initWidget(layoutPanel);
+		
+		displayHeader();
+	}
+
+	private void displayHeader() {
+		
+		WidgetStyleUtils.setStyleToWidget(layoutPanel, "header");
+		
+		//1. logo
+		Panel logoPanel = WidgetStyleUtils.createFlowPanelWithStyles( "logo" );		
+		String logoURL = GWTExtClientUtils.getMappingElem().getPrefix() + "/gwtreservation";
+		Anchor logoAnchor = WidgetStyleUtils.createExternalLink("gogwt logo", logoURL);		
+		logoAnchor.setText( "" );
+		
+		Image logoImage = new Image();
+		final String contextPath = GWTExtClientUtils.getMappingElem().getContextPath();
+		logoImage.setUrl(contextPath + "/images/logo.JPG" );
+		
+		//add logo to logoAnchor
+		logoAnchor.getElement().appendChild( logoImage.getElement() );		
+		logoPanel.add(logoAnchor);
+				
+		layoutPanel.add(logoPanel);
+		
+		// text panel
+		Panel headerTextPanel = WidgetStyleUtils.createHorizontalPanel();
+		headerTextPanel.addStyleName("headerText");
+		Label rightLabel = new Label(" GWT, Spring MVC, Hibernate, AOP, SEO, Performance Tuneup ");
+		rightLabel.addStyleName("rightLabel");
+		headerTextPanel.add(rightLabel);
+		
+		layoutPanel.add(headerTextPanel);
+		
+		TagsReservationLookupResources tagLookup = TagsReservationLookupResources.Util.getInstance();
+		// language text
+		String langSupportList = tagLookup.language_support_list();
+		
+		String [] langRegions = langSupportList.split(",");
+		
+		String currentLangRegion = GWTExtClientUtils.getMappingElem().getLanguageId() + "_" + GWTExtClientUtils.getMappingElem().getCountryId().toUpperCase();
+		
+		Panel languagePanel = WidgetStyleUtils.createHorizontalPanel();
+		languagePanel.addStyleName("headerLanguage");
+		
+		HTML langLinks = new HTML();
+		
+		int i=0;
+		for (String lage_Region : langRegions) {
+			if (!GWTStringUtils.equalsIgnoreCase(currentLangRegion, lage_Region)) {
+				String langName = tagLookup.getString("language_selector_" + lage_Region);
+				String langLink = contructLanguageLink(lage_Region);
+			 
+				langLinks = WidgetStyleUtils.createHtmlLink(langName, langLink);
+				languagePanel.add(langLinks);
+				
+				//for zh_CN, display not support message
+ 				if (GWTStringUtils.equalsIgnoreCase(lage_Region, "zh_CN")) {
+					langLinks.addClickHandler(new ClickHandler() {
+
+						public void onClick(ClickEvent arg0) {
+							Window.alert(" Not Support Yet, just demo how to switch languages ");							 
+						}					 
+					});
+				}
+				
+				if (++i != langRegions.length-1) {
+					languagePanel.add(WidgetStyleUtils.createStyledLabel( "|", "veritcalDivider" ));
+				}
+			}
+		}
+ 
+		layoutPanel.add(languagePanel);
+	 
+	}
+	
+ 
+	private String contructLanguageLink(final String lage_Region) {
+		StringBuilder sbuilder = new StringBuilder();
+		sbuilder.append(GWTExtClientUtils.getMappingElem().getContextPath());
+		sbuilder.append(SLASH);
+		String lageRegion = lage_Region.replace(UNDER_SCORE, HYPHON);
+		sbuilder.append(lageRegion);
+		sbuilder.append(SLASH);
+		//sbuilder.append(GWTExtClientUtils.getMappingElem().getControllerName());
+		sbuilder.append("gwtreservation");
+		
+		//add request if any
+		final String queryString = Window.Location.getQueryString();
+		if (GWTStringUtils.isSet(queryString)) {
+			//sbuilder.append("?");
+			sbuilder.append(queryString);
+		}
+		return sbuilder.toString();
+	}
+}
