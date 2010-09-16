77d6e9d39e57f23a936d903215c3f049ffb3618f
allhotelmotel <allhotelmotel@1e24c64f-693a-f468-3ac7-b5590d3b4d65>
2010-09-16 18:59:27 +0000

    Sugeestion box Google map

--- trunk/gwtbooking/src/main/java/com/gogwt/app/booking/gwt/common/utils/GoogleUtils.java
+++ trunk/gwtbooking/src/main/java/com/gogwt/app/booking/gwt/common/utils/GoogleUtils.java
@@ -1,18 +1,38 @@
 package com.gogwt.app.booking.gwt.common.utils;
 
 import com.google.gwt.maps.client.Maps;
+import com.google.gwt.user.client.Window;
 
 
 public final class GoogleUtils {
 	private static boolean googleMapKeyLoaded;
 
-	public final static String GOOGLE_MAPKEY="ABQIAAAAmdQW6A4k5xzDjjGKjkBoGxTNnYUpi0vJmWXtC66R5nqEvj2QuxSWoIfZsVFbr3by_GoWAub_tmLJLA";
-	
+	public final static String GOOGLE_MAPKEY_GOGWT ="ABQIAAAAmdQW6A4k5xzDjjGKjkBoGxTNnYUpi0vJmWXtC66R5nqEvj2QuxSWoIfZsVFbr3by_GoWAub_tmLJLA";
+	public final static String GOOGLE_MAPKYE_ALLHOTELMOTEL = "ABQIAAAAmdQW6A4k5xzDjjGKjkBoGxRuL81oiDTDYRFw2ophuZYKEuHgiBS2WD2rY2gMxFpirkx_QeTPC8zx0A";
 	
 	public interface KeyReady{
 		public void showMap();
 	}
 	
+	public static String getGoogleMapKey() {
+		String hostName = Window.Location.getHostName();
+		
+		if (hostName == null) {
+			return GOOGLE_MAPKEY_GOGWT;
+		}
+		
+		int pos = hostName.indexOf("gogwt.com");
+		if (pos != -1) {
+			return GOOGLE_MAPKEY_GOGWT;
+		}
+		
+		pos = hostName.indexOf("allhotelmotel.com");
+		if (pos != -1) {
+			return GOOGLE_MAPKYE_ALLHOTELMOTEL;
+		}
+		
+		return GOOGLE_MAPKEY_GOGWT;
+	}
 	/**
 	 * Load Google Map Key
 	 * @param languageId
@@ -21,7 +41,7 @@ public final class GoogleUtils {
 	public static void loadGoogleMapKey(
 			final Object carriedValue, final KeyReady keyReadyCallBack) {
 		 
-		 Maps.loadMapsApi(GoogleUtils.GOOGLE_MAPKEY, "2", false, new Runnable() {
+		 Maps.loadMapsApi(GoogleUtils.getGoogleMapKey(), "2", false, new Runnable() {
 		      public void run() {
 		    	  //showMap(hotelSearchResponseBean);
 		      }
@@ -30,7 +50,7 @@ public final class GoogleUtils {
 
 	public static void loadGoogleMapKey() {
 	   if (!googleMapKeyLoaded) {
-		   Maps.loadMapsApi(GoogleUtils.GOOGLE_MAPKEY, "2", false, new Runnable() {
+		   Maps.loadMapsApi(GoogleUtils.getGoogleMapKey(), "2", false, new Runnable() {
 		      public void run() {
 		    	  googleMapKeyLoaded = true;
 		      }
