cdd79e68f46e5ae8eb8b365f4f90d05ef30626d6
allhotelmotel <allhotelmotel@1e24c64f-693a-f468-3ac7-b5590d3b4d65>
2010-10-15 14:11:26 +0000

    Add gogwtarch application

---
+++ trunk/gogwtarch/src/main/java/com/gogwt/framework/arch/widgets/AbstractPage.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2010 GoGWT.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gogwt.framework.arch.widgets;
+
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.Panel;
+
+public abstract class AbstractPage extends Composite {
+	protected final Panel pagePanel;
+	 
+	public AbstractPage() {
+		pagePanel = new FlowPanel();
+		  initWidget(pagePanel);
+    }
+	
+	/**
+	 *  sub class should implemente it
+	 */
+	public abstract void process();
+	
+	public void preProcess() {}
+	public void postProcess() {}
+}
