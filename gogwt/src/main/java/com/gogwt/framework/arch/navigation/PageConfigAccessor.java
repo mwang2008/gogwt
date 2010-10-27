9f75e453f89241f7c815adc5ce66d8e5184719c9
allhotelmotel <allhotelmotel@1e24c64f-693a-f468-3ac7-b5590d3b4d65>
2010-10-27 16:44:11 +0000

    copy from gogwtarch.

---
+++ gogwt/src/main/java/com/gogwt/framework/arch/navigation/PageConfigAccessor.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2010 GoGWT.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.gogwt.framework.arch.navigation;
+
+import java.util.Map;
+
+public interface PageConfigAccessor {
+	 public String toString();
+	   
+	   /**
+	    * <p>
+	    * Instantiates the view widgets involved in a particular navigation mapping
+	    * If the view has already been instantiated, returns this instance or create a new one
+	    * </p>
+	    * @param token
+	    * @return
+	    */
+	   PageConfig lazyCreateOrGetPageConfig( String token );
+
+	   /**
+	    * <p>
+	    * Provides list of view tokens
+	    * </p>
+	    * @return
+	    */
+	   String[] getPageTokens();
+	   
+	   /**
+	    * <p>
+	    * Keeps track of instances of the view widgets associated in a
+	    * particular navigation mapping
+	    * </p>
+	    * @return
+	    */
+	   Map<String, PageConfig> getPageConfigInstances();
+}
