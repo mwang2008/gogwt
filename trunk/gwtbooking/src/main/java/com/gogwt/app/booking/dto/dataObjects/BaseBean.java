62c86c0d6ab1b736d0fc55af295498509ab34c25
allhotelmotel <allhotelmotel@1e24c64f-693a-f468-3ac7-b5590d3b4d65>
2010-12-23 16:23:14 +0000

    delete

--- trunk/gwtbooking/src/main/java/com/gogwt/app/booking/dto/dataObjects/BaseBean.java
+++
@@ -1,9 +0,0 @@
-package com.gogwt.app.booking.dto.dataObjects;
-
-import com.google.gwt.user.client.rpc.IsSerializable;
-
-public class BaseBean implements IsSerializable {
-	/*public String toString() {
-		return StringUtils.toString(this);
-	}*/
-}
