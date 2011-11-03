689b02d556e50d779af100a25bb15de285d7c8c2
allhotelmotel <allhotelmotel@1e24c64f-693a-f468-3ac7-b5590d3b4d65>
2011-11-03 15:25:31 +0000

    Add google app engine for gogwt mobile tracking app. Used to send email

---
+++ trunk/GoGWTAppEngine/myreadme.txt
@@ -0,0 +1,13 @@
+Install Google appengine for sending email function. 
+Download helio eclipse and install plug http://dl.google.com/eclipse/plugin/3.6
+
+1. create gogwtengine with title Go GWT Communication Email  in google app engine. 
+2. https://appengine.google.com/  allhotelmote@gmail.com freedog
+3. add <application>gogwtengine</application> to appengine-web.xml
+4. This project is in http://gogwtengine.appspot.com/
+
+Run: 
+local: right click MyGAELocation --> Run As --> Web Application
+
+Push to App Engine
+right click --> Google --> Deploy To App Engine
---
+++ trunk/GoGWTAppEngine/src/com/gogwt/appengine/apps/communication/GoGwtAppEngineServlet.java
@@ -0,0 +1,33 @@
+package com.gogwt.appengine.apps.communication;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class GoGwtAppEngineServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		String from = "no-reply-gogwt@gmail.com";
+		
+		List<String> to = new ArrayList<String>();
+		to.add("mwang_2008@yahoo.com");
+		to.add("michael.wang@cypresscare.com");
+		
+		String subject = " test app engine email";
+	    String body = "test";
+	    
+		boolean success = SendEmailUtils.gogwtNotificationEmail(to, subject, body);
+		
+		if (success) {
+			resp.getWriter().println("Success sending email");
+		}
+		else {
+			resp.getWriter().println("Fail to send email");
+		}
+		resp.getWriter().println("Hello, world");
+	}
+}
