689fc0a3d0167f02dc9e692ef404655694e1f43c
allhotelmotel <allhotelmotel@1e24c64f-693a-f468-3ac7-b5590d3b4d65>
2012-01-06 20:50:01 +0000

    add email template enrollemail_template.html forward in index.jsp

--- GoGWTrackingServer/src/main/java/com/gogwt/apps/tracking/services/communication/NotificationEmail.java
+++ GoGWTrackingServer/src/main/java/com/gogwt/apps/tracking/services/communication/NotificationEmail.java
@@ -1,43 +1,70 @@
 package com.gogwt.apps.tracking.services.communication;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.io.IOUtils;
 import org.apache.log4j.Logger;
 
 import com.gogwt.apps.tracking.data.CustomerProfile;
 import com.gogwt.apps.tracking.services.domain.ProfileBusinessDomainService;
 
 public class NotificationEmail {
-	private static Logger logger = Logger.getLogger(ProfileBusinessDomainService.class);
-	
-	private static final String emailURL = "http://gogwtengine.appspot.com/notifyemail";
-	
-    public static void sendEnrollEmail(final CustomerProfile profile) {
-    	logger.debug("=sendEnrollEmail");
-    	
-    	HttpClient client = new HttpClient();
-    	PostMethod method = new PostMethod(emailURL);
-    	
-    	NameValuePair[] data = {
-    	    new NameValuePair("to", profile.getEmail()),
-    	    new NameValuePair("subject", "Welcome to enroll to gogwt mobile app"),
-    	    new NameValuePair("body", "Success Enroll")
-    	};
-    	method.setRequestBody(data);
-    	
-    	try {
+	private static Logger logger = Logger
+			.getLogger(ProfileBusinessDomainService.class);
+
+	// private static final String emailURL =
+	// "http://gogwtengine.appspot.com/notifyemail";
+	private static final String emailURL = "http://gogwtmail.appspot.com/notifyemail";
+	private String EMAIL_TEMPLATE = "/enrollemail_template.html";
+
+	public void sendEnrollEmail(final CustomerProfile profile) {
+		logger.debug("=sendEnrollEmail");
+
+		HttpClient client = new HttpClient();
+		PostMethod method = new PostMethod(emailURL);
+		try {
+			NameValuePair[] data = {
+					new NameValuePair("to", profile.getEmail()),
+					new NameValuePair("subject",
+							"Welcome to choose gogwt mobile app"),
+					new NameValuePair("body", constructBody(profile)) };
+			method.setRequestBody(data);
+
 			int statusCode = client.executeMethod(method);
 		} catch (HttpException e) {
 			e.printStackTrace();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			method.releaseConnection();
+		}
+	}
+
+	private String constructBody(final CustomerProfile profile) {
+		InputStream in = this.getClass().getResourceAsStream(EMAIL_TEMPLATE);
+		try {
+			String body = IOUtils.toString(in, "UTF-8");
+			body = body.replaceAll("\\$\\{firstname\\}", profile.getFirstName());
+
+			body = body.replaceAll("\\$\\{lastname\\}", profile.getLastName());
+			body = body.replaceAll("\\$\\{userName\\}", profile.getUserName());
+			body = body.replaceAll("\\$\\{groupId\\}", profile.getGroupId());
+			body = body.replaceAll("\\$\\{password\\}", profile.getPassword());
+			body = body.replaceAll("\\$\\{email\\}", profile.getEmail());
+
+			return body;
+
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-        finally {
-           method.releaseConnection();
-        }
-    }
+
+		return null;
+	}
+
 }
