6adfcc70bd3c6fdda200b906a78c71b4fc465300
allhotelmotel <allhotelmotel@1e24c64f-693a-f468-3ac7-b5590d3b4d65>
2012-10-02 20:08:42 +0000

    Add Rest Template to retrieve Rest weather request.

---
+++ GoGWTWebservice/src/main/java/com/gogwt/utils/ToStringUtils.java
@@ -0,0 +1,21 @@
+package com.gogwt.utils;
+
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.StandardToStringStyle;
+
+public class ToStringUtils {
+	/**
+     * Provides standard toString method for domain objects.
+     *
+     * @param object - Object to evaluate.
+     *
+     * @return String representation of object.
+     */
+   public static String toString( final Object object ) {
+        StandardToStringStyle toStringStyle = new StandardToStringStyle();
+        toStringStyle.setUseShortClassName( true );
+
+        return ReflectionToStringBuilder.toString( object, toStringStyle );
+    }
+}
+
